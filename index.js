1915beec06ff010cb6b0f5034d78ef327c2c7209
Renne Jaskonis <rjaskonis@gmail.com>
2020-12-10 12:21:49 -0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = function Hello() {
-    console.log("Hello World");
+const express = require("express");
+const app = express();
+
+module.exports = function Hello(settings) {
+    if (!settings) {
+        settings = {
+            portnumber: 3333,
+        };
+    }
+
+    app.listen(settings.portnumber, () =>
+        console.log(
+            "\x1b[36m%s\x1b[0m",
+            `**Server listening on port ${settings.portnumber}**`
+        )
+    );
 };
