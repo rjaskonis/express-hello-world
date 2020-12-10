02c35f8a3120337bc42cf8fd1f8d8ef665051a9e
Renne Jaskonis <rjaskonis@gmail.com>
2020-12-10 12:32:49 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.send("Hello World!"));
+
 module.exports = function Hello(settings) {
     if (!settings) {
         settings = {
